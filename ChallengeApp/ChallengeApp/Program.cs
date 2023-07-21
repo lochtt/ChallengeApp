456bda686c40d7f5453e05ea1898c3f5102e7b6c
lochtt <ulmek18@gmail.com>
2023-07-21 07:37:15 +0200

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Marcin";
+var age = 28;
+bool isWoman = true ;
+
+if (isWoman == true && age < 30) 
+{
+    Console.WriteLine("Kobieta w wieku poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa,lat 30");
+}
+else if (isWoman == false && age < 18)
+{
+    Console.WriteLine("Niepenoletni mężczyzna");
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
